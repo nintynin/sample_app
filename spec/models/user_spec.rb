dc66ec7af7bbd0fd02839ef3fd44e47e4bc591e6
sungkyung kim <hardcoredesign@gmail.com>
2011-05-28 03:11:56 +0900

    User signup complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -91,13 +91,13 @@ describe User do
     
     it "should reject short passwords" do
       short = "a" * 5
-      hash = @attr.merge(:password => short, :password_confirmation => short,)
+      hash = @attr.merge(:password => short, :password_confirmation => short)
       User.new(hash).should_not be_valid
     end
     
     it "should reject long passwords" do
       long = "a" * 41
-      hash = @attr.merge(:password => long, :password_confirmation => long,)
+      hash = @attr.merge(:password => long, :password_confirmation => long)
       User.new(hash).should_not be_valid
     end
     
